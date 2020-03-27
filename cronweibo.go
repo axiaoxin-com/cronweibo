6c02d5bf206fdd53e86ecc6d1753c8b60949f5ed
axiaoxin <254606826@qq.com>
2020-03-27 22:09:46 +0800

    godoc online

--- cronweibo.go
+++ cronweibo.go
@@ -1,5 +1,7 @@
-// Package cronweibo 提供定时发送微博相关功能
-// 注册需要定时执行的微博任务函数，服务运行后将按注册的定时规则定时执行任务，并将任务执行结果发送到微博
+// Package cronweibo 提供简单的方法便于快速开发定时发送微博的应用
+//
+// 实现一个定时发送微博的程序你只需实现具体的任务函数，
+// 生成对应的任务后，将其注册到服务中，服务运行后将按注册的定时规则定时执行任务中的执行函数，并将执行结果发送到微博
 package cronweibo
 
 import (
@@ -14,7 +16,7 @@ import (
 	"github.com/robfig/cron"
 )
 
-// CronWeibo 定时微博结构体
+// CronWeibo 定时微博服务定义
 type CronWeibo struct {
 	weibo            *weibo.Weibo
 	token            *weibo.TokenResp
@@ -26,18 +28,18 @@ type CronWeibo struct {
 	location         *time.Location
 }
 
-// Config New函数配置信息
+// Config CronWeibo配置定义，New函数的参数
 type Config struct {
 	// 微博相关配置
-	WeiboAppkey         string
-	WeiboAppsecret      string
-	WeiboRedirecturi    string
-	WeiboUsername       string
-	WeiboPasswd         string
-	WeiboSecurityDomain string
-	WeiboPinCrackFuncs  []weibo.CrackPinFunc
+	WeiboUsername       string               // 要发微博的微博登录账号（必填参数，用于模拟登录自动获取授权码）
+	WeiboPasswd         string               // 要发微博的微博登录密码（必填参数，用于模拟登录
+	WeiboPinCrackFuncs  []weibo.CrackPinFunc // 登录验证码破解函数（非必填）
+	WeiboAppkey         string               // 微博应用的appkey（必填参数）
+	WeiboAppsecret      string               // 微博应用的appsecret（必填参数）
+	WeiboRedirecturi    string               // 微博应用的回调地址（必填参数）
+	WeiboSecurityDomain string               // 微博应用的安全域名（必填参数）
 	// cron相关配置
-	Location *time.Location
+	Location *time.Location // 指定定时服务的时区（非必填）
 }
 
 // WeiboJobFunc 微博任务函数类型声明
@@ -47,19 +49,18 @@ type WeiboJobFunc func() (string, io.Reader)
 // WeiboPinCrackFunc 微博验证码破解函数类型声明
 type WeiboPinCrackFunc weibo.CrackPinFunc
 
-// WeiboJob 微博任务结构，任务名 + 定时表达式 + 任务函数
-/* schedule:
-Entry                  | Description                                | Equivalent To
------                  | -----------                                | -------------
-@yearly (or @annually) | Run once a year, midnight, Jan. 1st        | 0 0 0 1 1 *
-@monthly               | Run once a month, midnight, first of month | 0 0 0 1 * *
-@weekly                | Run once a week, midnight between Sat/Sun  | 0 0 0 * * 0
-@daily (or @midnight)  | Run once a day, midnight                   | 0 0 0 * * *
-@hourly                | Run once an hour, beginning of hour        | 0 0 * * * *
-*/
+// WeiboJob 微博任务定义，任务名 + 定时表达式 + 任务函数组成☝️任务
 type WeiboJob struct {
-	Name     string       // 任务名称
+	/* Schedule 格式参考：
+	   Entry                  | Description                                | Equivalent To
+	   @yearly (or @annually) | Run once a year, midnight, Jan. 1st        | 0 0 0 1 1 *
+	   @monthly               | Run once a month, midnight, first of month | 0 0 0 1 * *
+	   @weekly                | Run once a week, midnight between Sat/Sun  | 0 0 0 * * 0
+	   @daily (or @midnight)  | Run once a day, midnight                   | 0 0 0 * * *
+	   @hourly                | Run once an hour, beginning of hour        | 0 0 * * * *
+	*/
 	Schedule string       // 定时任务表达式
+	Name     string       // 任务名称
 	Run      WeiboJobFunc // 需要执行的微博任务函数
 }
 
@@ -105,13 +106,15 @@ func New(config *Config, weiboJobs ...WeiboJob) (*CronWeibo, error) {
 	return cw, nil
 }
 
-// Now 获取配置时区的当前时间
+// Now 获取CronWeibo中的当前时间
+// 应用中需要获取当前时间请使用该方法保证时间时区正确
 func (c *CronWeibo) Now() time.Time {
 	now := time.Now().In(c.location)
 	return now
 }
 
 // UpdateToken 检查access_token是否过期，过期则更新
+// 一般情况无需使用到，默认在注册任务后执行任务时会自动检查
 func (c *CronWeibo) UpdateToken() error {
 	// 互斥锁
 	c.tokenUpdateMutex.Lock()
@@ -162,7 +165,7 @@ func (c *CronWeibo) cronFuncFactory(weiboJob WeiboJob) cron.FuncJob {
 	return cronFunc
 }
 
-// RegisterWeiboJobs 注册微博任务函数
+// RegisterWeiboJobs 注册微博任务
 func (c *CronWeibo) RegisterWeiboJobs(weiboJobs ...WeiboJob) {
 	for _, job := range weiboJobs {
 		// job转换为cronFunc
--- go.mod
+++ go.mod
@@ -3,10 +3,7 @@ module github.com/axiaoxin-com/cronweibo
 go 1.13
 
 require (
-	github.com/axiaoxin-com/chaojiying v1.0.0
 	github.com/axiaoxin-com/weibo v1.0.0
-	github.com/axiaoxin/weibo v1.0.0
 	github.com/pkg/errors v0.9.1
 	github.com/robfig/cron v1.2.0
-	github.com/spf13/viper v1.6.2
 )
